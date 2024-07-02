56b2c2072c8e114ce59f62af6666efc1a564849d
annaritadituri <annaritadituri@icloud.com>
2024-07-02 20:42:56 +0200

    [UPD] Added query (JOIN)

--- university.sql
+++ university.sql
@@ -25,16 +25,37 @@ FROM `degrees`
 GROUP BY `department_id`;
 
 --JOIN
+
 --1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
---2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di
---Neuroscienze
+
+SELECT *
+FROM `degrees`
+INNER JOIN `students`
+ON `students`.`degree_id` = `degrees`.`id`
+WHERE `degrees`.`name` = "Corso di Laurea in Economia";
+
+--2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+
+SELECT *
+FROM `departments`
+INNER JOIN 	`degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `degrees`.`level` = "magistrale"
+AND `departments`.`name` = "Dipartimento di Neuroscienze";
+
 --3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
---4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui
---sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e
---nome
+
+
+--4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+SELECT *
+FROM `students`
+INNER JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+INNER JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`;
+
 --5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
---6. Selezionare tutti i docenti che insegnano nel Dipartimento di
---Matematica (54)
---7. BONUS: Selezionare per ogni studente il numero di tentativi sostenuti
---per ogni esame, stampando anche il voto massimo. Successivamente,
---filtrare i tentativi con voto minimo 18.
+--6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+--7. BONUS: Selezionare per ogni studente il numero di tentativi sostenuti per ogni esame, stampando anche il voto massimo. Successivamente, filtrare i tentativi con voto minimo 18.
