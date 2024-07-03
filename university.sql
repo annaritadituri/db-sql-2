ab0188815d7dc1d5f7bed398b0c70b8eebabce56
annaritadituri <annaritadituri@icloud.com>
2024-07-03 21:44:52 +0200

    [UPD] Added bonus query (JOIN)

--- university.sql
+++ university.sql
@@ -45,6 +45,13 @@ AND `departments`.`name` = "Dipartimento di Neuroscienze";
 
 --3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
+SELECT *
+FROM `teachers`
+JOIN `course_teacher`
+ON `course_teacher`.`teacher_id` = `teachers`.`id`
+JOIN `courses`
+ON `course_teacher`.`course_id` = `courses`.`id`
+WHERE `teachers`.`id` = 44;
 
 --4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
@@ -56,6 +63,42 @@ INNER JOIN `departments`
 ON `degrees`.`department_id` = `departments`.`id`;
 
 --5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+SELECT *
+FROM `degrees`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`;
+
 --6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
+SELECT DISTINCT `teachers`.*
+FROM `departments`
+JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+WHERE `departments`.`name` = "Dipartimento di Matematica";
+
 --7. BONUS: Selezionare per ogni studente il numero di tentativi sostenuti per ogni esame, stampando anche il voto massimo. Successivamente, filtrare i tentativi con voto minimo 18.
+
+SELECT
+	`students`.`name`,
+    `students`.`surname`,
+	COUNT(`exam_student`.`vote`) AS `done`,
+    MAX(`exam_student`.`vote`) AS `max_vote`,
+    MIN(`exam_student`.`vote`) AS `min_vote`
+FROM `students`
+JOIN `exam_student`
+ON `students`.`id` = `exam_student`.`student_id`
+JOIN `exams`
+ON `exam_student`.`exam_id` = `exams`.`id`
+GROUP BY `exams`.`course_id`,`students`.`id`
+HAVING `min_vote` >= 18;
